b401afba8e5678c76b51deaa8b482d90bdbc8252
Criste Phillips <criste.phillips@yahoo.com>
2023-08-31 13:18:58 -0400

    user api routes bricked out and index js file added to api directory

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -34,12 +34,60 @@ router.get('/:id', (req, res)=> {
 });
 
 // POST / api / users
-router.post('/', (req, res)=> {});
+router.post('/', (req, res)=> {
+    // expects {username: '', email: '', password: ''}
+    // .create() inserts data & passes in key expected key values
+    User.create({
+        unsername: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
-// PUT / api / users /1
-router.put('/:id', (req, res)=>{});
+// PUT / api / users /1 to update existing data
+router.put('/:id', (req, res)=>{
+    // expects {username: '', email: '', password: ''}
+    // if req.body has exact key/value pairs to match model, can use `req.body` instead
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    }) 
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user has been found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // DELETE / api / users /1
-router.delete('/:id', (req, res)=>{});
+router.delete('/:id', (req, res)=>{
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({ message: 'No user has been foudn with this id' });
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
