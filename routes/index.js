f3e1c6a2efdf54f602c9924dc36868b105634841
Criste Phillips <criste.phillips@yahoo.com>
2023-08-31 13:21:35 -0400

    index file added to routes to collect api routes packaged within api index file

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+// collect packaged api routes from routes/api/index.js
+
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
