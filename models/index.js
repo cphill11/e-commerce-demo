cfa5bedef77180ffc0bee2c79633e3268ed710d1
Criste Phillips <criste.phillips@yahoo.com>
2023-09-11 13:33:23 -0400

    initial routes for comments in place

--- models/index.js
+++ models/index.js
@@ -42,4 +42,20 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
 module.exports = { User, Post, Vote, Comment };
