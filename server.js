dd36f1d4c5605b821f4c789c315792aaace9baab
Criste Phillips <criste.phillips@yahoo.com>
2023-09-14 19:24:39 -0400

    user routes updated to utilize sessions for POST functionalities

--- server.js
+++ server.js
@@ -3,26 +3,25 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 
-const hbs = exphbs.create({});
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+const routes = require('./controllers');
 
-const SequilizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+const SequelizeStore= require('connect-session-sequelize')(session.Store);
+
 const sess = {
     secret: 'Super secret type secret',
     cookie: {},
     saveUnitialized: true,
-    store: newSequelizeStore({
+    store: new SequelizeStore({
         db: sequelize
     })
 };
 
+app.use(session(sess));
 
-
-
+const hbs = exphbs.create({});
 // connects to handlebar template; handlebar template lives in view/layouts/main.handlebars
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -30,7 +29,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session(sess));
+
 
 // turn on routes (controllers)
 app.use(routes);
